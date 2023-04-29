a2350eab200bd940c4af8ea246299a81f40598fe
John Roberts <bigjlr2004@yahoo.com>
2023-04-28 20:35:15 -0500

    Added Store Model Class

---
+++ Get2Work/Models/Store.cs
@@ -0,0 +1,12 @@
+﻿namespace Get2Work.Models
+{
+    public class Store
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public bool ActiveStatus { get; set; }
+
+    }
+}
